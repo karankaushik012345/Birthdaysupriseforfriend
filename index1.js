c96cf415efa1bd33d15780195f6ae990ffca33ac
karankaushik012345 <karan_ug_23@ece.nits.ac.in>
2024-08-23 21:51:48 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,1 +1,11 @@
 
+
+document.getElementById('giftBox').addEventListener('click', function() {
+    const lid = document.getElementById('giftLid');
+    lid.style.transform = 'rotateX(90deg)';
+
+    const surpriseMessage = document.getElementById('surpriseMessage');
+    setTimeout(() => {
+        surpriseMessage.style.display = 'block';
+    }, 500);
+});
